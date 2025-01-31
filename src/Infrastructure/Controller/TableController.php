6d7c035e24c0fe8ca193c4136db715156b3b5f26
Mathieu <mathieu.boucher@epsi.fr>
2025-01-31 17:18:24 +0100

    add swagger attributes for TableController

--- src/Infrastructure/Controller/TableController.php
+++ src/Infrastructure/Controller/TableController.php
@@ -2,18 +2,21 @@
 
 namespace App\Infrastructure\Controller;
 
+use DateTime;
+use OpenApi\Attributes as OA;
+use Nelmio\ApiDocBundle\Attribute\Model;
 use Symfony\Component\HttpFoundation\Request;
+use App\Application\DTO\Table\TableRequestDTO;
 use Symfony\Component\HttpFoundation\Response;
+use App\Application\DTO\Table\TableResponseDTO;
+use App\Application\Port\TableUseCaseInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use App\Application\Port\TableUseCaseInterface;
-use App\Application\DTO\Table\TableRequestDTO;
-use App\Application\DTO\Table\TableResponseDTO;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Translation\Exception\NotFoundResourceException;
-use DateTime;
 
+#[OA\Tag(name: "Tables")]
 class TableController extends AbstractController
 {
     private TableUseCaseInterface $tableService;
@@ -28,6 +31,22 @@ class TableController extends AbstractController
     }
 
     #[Route('/tables', methods: ['POST'])]
+    #[OA\Post(
+        path: "/tables",
+        summary: "Add a new table to a restaurant",
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\JsonContent(ref: new Model(type: TableRequestDTO::class))
+        ),
+        responses: [
+            new OA\Response(
+                response: 201,
+                description: "Table created",
+                content: new OA\JsonContent(ref: new Model(type: TableResponseDTO::class))
+            ),
+            new OA\Response(response: 400, description: "Invalid input data")
+        ]
+    )]
     public function addTable(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
@@ -57,6 +76,27 @@ class TableController extends AbstractController
     }
 
     #[Route('/tables/{id}/availability', methods: ['GET'])]
+    #[OA\Get(
+        path: "/tables/{id}/availability",
+        summary: "Check if a table is available for a given time range",
+        parameters: [
+            new OA\Parameter(name: "id", in: "path", required: true, schema: new OA\Schema(type: "integer")),
+            new OA\Parameter(name: "startTime", in: "query", required: true, schema: new OA\Schema(type: "string", format: "date-time"), example: "2025-02-01 19:00:00"),
+            new OA\Parameter(name: "endTime", in: "query", required: true, schema: new OA\Schema(type: "string", format: "date-time"), example: "2025-02-01 21:00:00")
+        ],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "Availability status",
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(property: "available", type: "boolean", example: true)
+                    ]
+                )
+            ),
+            new OA\Response(response: 404, description: "Table not found")
+        ]
+    )]
     public function isTableAvailable(int $id, Request $request): JsonResponse
     {
         $startTime = new DateTime($request->query->get('startTime'));
